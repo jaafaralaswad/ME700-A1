f0f7435ced98df5ff1f42396a224c5256d86b5d7
Jaafar Alaswad <jalaswad@bu.edu>
2025-02-02 17:28:26 -0500

    Add setup file

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bisection',           # Name of your package
+    version='0.1',              # Version number
+    packages=find_packages(),   # Automatically find all packages and subpackages
+)
